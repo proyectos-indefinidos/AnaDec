7619c9cd998b528b04e212768a4eb03b3835f040
alejandroabrilm <d.abrilm@uniandes.edu.co>
2026-01-18 20:38:37 -0500

    cambios en convertidor.py para mejorar entendibilidad de codigo, como cambiarTemporalidadEnEfectivo, e implementacion de la funcion, usando como base teorica la diapositiva adjunta en la nueva carpeta "documentation".

--- src/financeCore/convertidor.py
+++ src/financeCore/convertidor.py
@@ -1,13 +1,34 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+from numpy import rint
+
+
 class Convertidor:
     """Convertidor de tasas de interés con precisión de 6 dígitos."""
     def __init__(self):
         self.precision = 6
+#El siguiente método convierte una tasa de interés de una temporalidad a otra
+#Los params, funcionan de la siguiente manera:
+#tasa: es la tasa de interés que se quiere convertir
+#periodo_actual: es la temporalidad en la que se encuentra la tasa (puede ser 1(mes), 3(trimestre), 6(semestre), 12(año))
+#
+#nuevo_periodo:
+    def cambiarTemporalidadEnEfectivo(self, tasa, periodo_actual, nuevo_periodo):
+        """cambia la temporalidad de una tasa en efectivo a otra."""
+        if periodo_actual <= 0 and nuevo_periodo <= 0:
+            raise ValueError("Los períodos deben ser positivos y estar en la misma unidad")
+        if tasa <= -1:
+            raise ValueError("La tasa efectiva no puede ser <= -100%")
+        if periodo_actual == nuevo_periodo:
+            return round(tasa, self.precision)
+        i_nuevo = (1 + tasa) ** (nuevo_periodo / periodo_actual) - 1
+        return round(i_nuevo, self.precision)
+    
+#TODO: toca sacar de alguna forma la inflacion a la hora de cambiar de frecuencia, para tener en cuenta si 
+#se quiere cambiar a una frecuencia mayor o menor de la que se tiene
 
-    def nom_a_ef(self, tasa):
-        """Convierte tasas mensuales a anuales (o viceversa)."""
+    def cambiarTemporalidadEnNominal(self, tasa):
         pass
 
     def cambiar_frecuencia(self, tasa, nuevo_periodo):
@@ -16,3 +37,7 @@ class Convertidor:
     def tasa_a_ea_std(self, tasa):
         """Convierte generalmente a Efectiva Anual(EA)."""
         pass
+
+if __name__ == "__main__":
+    convertidor = Convertidor()
+    print(convertidor.cambiarTemporalidadEnEfectivo(0.06, 3, 12))  # Ejemplo de uso, se puede revisar en las diapos, que voy a adjuntar, esta correcto!
